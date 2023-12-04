86cc4b09452496211c92f6b38f7eb035ff025f6e
quantmad <quantmad@gmail.com>
2023-12-04 21:25:16 +0200

    Добавлен функционал определения пользовательских символов. Добавлены методы переключения режимов переполнения дисплея. Добавлены примеры библиотеки:

--- D010107.cpp
+++ D010107.cpp
@@ -5,6 +5,65 @@ DPD201::DPD201(byte txPin, int baudrate = 9600) {
   _dpd_201.begin(baudrate);
 }
 
+void DPD201::defineUserChar(byte b1, byte b2, byte b3, byte b4, byte b5, byte codeLocation) {
+  if (codeLocation <= 0x20 || codeLocation >= 0x7E) return;
+  _dpd_201.write(_ESC);
+  _dpd_201.write(0x26); // &
+  _dpd_201.write(codeLocation);
+  _dpd_201.write(b1);
+  _dpd_201.write(b2);
+  _dpd_201.write(b3);
+  _dpd_201.write(b4);
+  _dpd_201.write(b5);
+}
+
+void DPD201::selectOverwriteMode() {
+  _dpd_201.write(_US);
+  _dpd_201.write(_MD1);
+}
+
+void DPD201::selectVerticalScrollMode() {
+  _dpd_201.write(_US);
+  _dpd_201.write(_MD2);
+}
+
+void DPD201::selectHorizontalScrollMode() {
+  _dpd_201.write(_US);
+  _dpd_201.write(_MD3);
+}
+
+void DPD201::defineUserChar(bool bits[], byte codeLocation) {
+  if (codeLocation <= 0x20 || codeLocation >= 0x7E) return;
+  _dpd_201.write(_ESC);
+  _dpd_201.write(0x26); // &
+  _dpd_201.write(codeLocation);
+
+  int bitNumber = 0;
+  char resultChar = 0;
+  for (int i = 0; i < 40; i++) {
+      resultChar += (bits[i] << (7 - bitNumber));
+      bitNumber++;
+
+      if (bitNumber == 8) {
+        bitNumber = 0;
+        _dpd_201.write(resultChar);
+        resultChar = 0;
+      }
+  }
+}
+
+void DPD201::selectUserCharSet(bool value) {
+  _dpd_201.write(_ESC);
+  _dpd_201.write(0x25); // %
+  _dpd_201.write(value ? 1 : 0);
+}
+
+void DPD201::writeUserChar(byte val) {
+  this->selectUserCharSet(1);
+  _dpd_201.write(val);
+  this->selectUserCharSet(0);
+}
+
 void DPD201::clearScreen() {
   _dpd_201.write(_CLR);
 }
@@ -47,7 +106,7 @@ void DPD201::switchAnnunciator(byte column, bool state) {
 
 void DPD201::cursorToPosition(byte row, byte column) {
   if (row < 1 || row > 2) return;
-  if (column > 0x14) return;
+  if (column < 1 || column > 0x14) return;
 
   _dpd_201.write(_US);
   _dpd_201.write(0x24); // $
@@ -74,6 +133,10 @@ void DPD201::write(byte val) {
   _dpd_201.write(val);
 }
 
+void DPD201::write(int val) {
+  _dpd_201.write(val);
+}
+
 void DPD201::cursorToRight() {
   _dpd_201.write(_US);
   _dpd_201.write(_CR);
@@ -86,10 +149,24 @@ void DPD201::cursorToEnd() {
 
 void DPD201::setBlinkInterval(byte val) {
   _dpd_201.write(_US);
-  _dpd_201.write(0x45);
+  _dpd_201.write(0x45); // E
   _dpd_201.write(val);
 }
 
 void DPD201::enableDisplay(bool enabled) {
   this->setBlinkInterval(enabled ? 0 : 255);
+}
+
+void DPD201::selectCharacterTable(byte n) {
+  if (n > 1) return;
+
+  _dpd_201.write(_ESC);
+  _dpd_201.write(0x74); // t
+  _dpd_201.write(n);
+}
+
+void DPD201::selectInternationalCharacterSet(byte code) {
+  _dpd_201.write(_ESC);
+  _dpd_201.write(0x52); // R
+  _dpd_201.write(code);
 }
--- D010107.h
+++ D010107.h
@@ -12,24 +12,34 @@
 #define _MD3 0x03
 
 class DPD201 {
-	public:
-		DPD201(byte txPin, int baudrate = 9600);
-		void clearScreen();
-		void selfTest();
-		void clearCursorLine();
-		void initializeDisplay();
-		void write(char* val);
+  public:
+    DPD201(byte txPin, int baudrate = 9600);
+    void clearScreen();
+    void selfTest();
+    void clearCursorLine();
+    void initializeDisplay();
+    void write(char* val);
     void write(char val);
     void write(byte val);
-		void brightnessAdjustment(byte val);  // 1 - 4
+    void write(int val);
+    void brightnessAdjustment(byte val);  // 1 - 4
     void resetAnnunciators();
-		void switchAnnunciator(byte column, bool state); 
+    void switchAnnunciator(byte column, bool state); 
     void cursorTo(byte position);
     void cursorToEnd();
     void cursorToRight();
-		void cursorToPosition(byte row, byte column);
+    void cursorToPosition(byte row, byte column);
     void setBlinkInterval(byte val);
     void enableDisplay(bool enabled);
-	private:
-		SoftwareSerial _dpd_201 = SoftwareSerial(255, 255);
+    void selectCharacterTable(byte n); // 0 - PC437, 1 - PC850
+    void selectInternationalCharacterSet(byte code);
+    void defineUserChar(bool bits[], byte codeLocation);
+    void defineUserChar(byte b1, byte b2, byte b3, byte b4, byte b5, byte codeLocation);
+    void selectUserCharSet(bool value);
+    void writeUserChar(byte val);
+    void selectOverwriteMode();
+    void selectVerticalScrollMode();
+    void selectHorizontalScrollMode();
+  private:
+    SoftwareSerial _dpd_201 = SoftwareSerial(255, 255); // Не знаю как более изящно обыграть этот момент, так что оставлю так :)
 };
